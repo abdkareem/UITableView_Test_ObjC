0c5b14b463ebc49d837d6d20f8178e75615300c0
root <abd.khaja@gmail.com>
2017-02-14 10:22:59 -0800

    Initial Commit

---
+++ UITableView_Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableView_Test
+//
+//  Created by Taiyaba Sultana on 2/14/17.
+//  Copyright © 2017 Abdul Kareem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableView_Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableView_Test
+//
+//  Created by Taiyaba Sultana on 2/14/17.
+//  Copyright © 2017 Abdul Kareem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
